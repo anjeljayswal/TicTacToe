6a57081fea391667e96908dfaeb95f2c2262ac0a
anjeljayswal <anjeljayswal@gmail.com>
2023-01-13 16:32:54 +0545

    changs

--- script.js
+++ script.js
@@ -14,21 +14,21 @@ const changeTurn =()=>{
 const checkWin  = () => {
     let boxtext = document.getElementsByClassName('boxtext');
     let wins = [
-        [0, 1, 2, 5, 5, 0],
-        [3, 4, 5, 5, 15, 0],
-        [6, 7, 8, 5, 25, 0],
-        [0, 3, 6, -5, 15, 90],
-        [1, 4, 7, 5, 15, 90],
-        [2, 5, 8, 15, 15, 90],
-        [0, 4, 8, 5, 15, 45],
-        [2, 4, 6, 5, 15, 135],
+        [0, 1, 2, 0, 5, 0],
+        [3, 4, 5, 0, 15, 0],
+        [6, 7, 8, 0, 25, 0],
+        [0, 3, 6, -10.5, 15, 90],
+        [1, 4, 7, -0.5, 15, 90],
+        [2, 5, 8, 9.5, 15, 90],
+        [0, 4, 8, 0, 15, 45],
+        [2, 4, 6, -1, 15, 135],
     ]
     wins.forEach(e=>{
         if((boxtext[e[0]].innerHTML === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "")){
             document.querySelector('.info').innerHTML = boxtext[e[0]].innerText + " won"
             isgameover = true;
             document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px"
-            document.querySelector('.line').style.width = '20vw';
+            document.querySelector('.line').style.width = '31vw';
             document.querySelector('.line').style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
             gameover.play()
         }
@@ -39,7 +39,7 @@ const checkWin  = () => {
 //Game Logic
 // music.play()
 let boxes = document.getElementsByClassName("box");
-music.play()
+// music.play()
 Array.from(boxes).forEach(element =>{
     let boxtext = element.querySelector('.boxtext')
     element.addEventListener('click', (e)=>{
